2577287cf7bf45c070d897271e041e7b811822cf
Marinko <marinkospasojevic@gmail.com>
2018-07-15 14:18:54 +0200

    Unit testing

---
+++ netcore-unit-testing/web-api/Contracts/IShoppingCartService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using web_api.Model;
+
+namespace web_api.Contracts
+{
+    public interface IShoppingCartService
+    {
+        IEnumerable<ShoppingItem> GetAllItems();
+        ShoppingItem Add(ShoppingItem newItem);
+        ShoppingItem GetById(Guid id);
+        void Remove(Guid id);
+    }
+}
---
+++ netcore-unit-testing/web-api/Controllers/ShoppingCartController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using web_api.Contracts;
+using web_api.Model;
+
+namespace web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        private readonly IShoppingCartService _service;
+
+        public ShoppingCartController(IShoppingCartService service)
+        {
+            _service = service;
+        }
+
+        // GET api/shoppingcart
+        [HttpGet]
+        public ActionResult<IEnumerable<ShoppingItem>> Get()
+        {
+            var items = _service.GetAllItems();
+            return Ok(items);
+        }
+
+        // GET api/shoppingcart/5
+        [HttpGet("{id}")]
+        public ActionResult<ShoppingItem> Get(Guid id)
+        {
+            var item = _service.GetById(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        // POST api/shoppingcart
+        [HttpPost]
+        public ActionResult Post([FromBody] ShoppingItem value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var item = _service.Add(value);
+            return CreatedAtAction("Get", new { id = item.Id }, item);
+        }
+
+        // DELETE api/shoppingcart/5
+        [HttpDelete("{id}")]
+        public ActionResult Remove(Guid id)
+        {
+            var existingItem = _service.GetById(id);
+
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            _service.Remove(id);
+            return Ok();
+        }
+    }
+}
