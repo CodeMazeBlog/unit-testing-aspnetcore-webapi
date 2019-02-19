b0dcf9478a34e0e449bfeeadb15e62e5030577e5
Marinko <marinkospasojevic@gmail.com>
2019-02-19 10:54:46 +0100

    Added support for .NET Core 2.2

---
+++ netcore-unit-testing/web-api/Controllers/ShoppingCartController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using web_api.Contracts;
+using web_api.Model;
+
+namespace web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        private readonly IShoppingCartService _service;
+
+        public ShoppingCartController(IShoppingCartService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ShoppingItem>> Get()
+        {
+            var items = _service.GetAllItems();
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ShoppingItem> Get(Guid id)
+        {
+            var item = _service.GetById(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] ShoppingItem value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var item = _service.Add(value);
+            return CreatedAtAction("Get", new { id = item.Id }, item);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Remove(Guid id)
+        {
+            var existingItem = _service.GetById(id);
+
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            _service.Remove(id);
+            return Ok();
+        }
+    }
+}
